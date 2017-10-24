909086a82534572e2a36393f26c018a457c6176e
Syfux <sylvaincarrier@orange.fr>
2017-10-24 08:28:37 +0200

    Init

---
+++ src/main/java/deqo/scar/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.scar.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
